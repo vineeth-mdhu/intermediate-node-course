949ea5991ad473d6080ac6066c43909702890f5b
Vineeth Madhu <vineethnbr13@gmail.com>
2021-08-28 23:53:51 +0530

    add user model and db connect

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const User= require('./models/User');
 const port=8000;
 const app= express();
 
+
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://node-express:node-express@cluster0.gs4ii.mongodb.net/node-express?retryWrites=true&w=majority');
+
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
